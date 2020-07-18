b47f7b7aa82b3686de7e469247273a495e4fd226
Tom Magorsch <tom@TmDebian>
2020-07-18 17:54:23 +0200

    Änderungen am test code - version für gpu ausrührung

--- code/test.py
+++ code/test.py
@@ -14,12 +14,12 @@ from tensorflow.compat.v1 import ConfigProto
 from tensorflow.compat.v1 import InteractiveSession
 
 config = ConfigProto()
-config.gpu_options.per_process_gpu_memory_fraction = 0.5
+config.gpu_options.per_process_gpu_memory_fraction = 0.75
 config.gpu_options.allow_growth = True
 session = InteractiveSession(config=config)
 
 train_dir = "afhq/train"
-image_num = 10000
+image_num = 14000
 val_split = 0.3
 
 X_train_filenames, X_val_filenames, y_train, y_val = load_data(train_dir, image_num, val_split)
@@ -30,33 +30,27 @@ my_training_batch_generator = Generator(X_train_filenames, y_train, batch_size)
 my_validation_batch_generator = Generator(X_val_filenames, y_val, batch_size)
 
 
-seq = Testsequence("Test1",1)
+seq = Testsequence("Test1",50)
 
-##Alles konstant Anzahl conv layer hoch
-#seq.add_model(1,64,2,32,5)
-#seq.add_model(2,64,2,32,3)
-#seq.add_model(3,64,2,32,2)
-#seq.add_model(4,64,2,32,2)
+#seq.add_model(5, [16,32,64,96,96], 1, [128],2)
+#seq.add_model(5, [16,32,64,96,96], 2, [64,32],2)
+#seq.add_model(5, [16,32,64,96,96], 2, [48,48],2)
+#seq.add_model(5, [16,32,64,96,96], 3, [48,32,16],2)
+#seq.add_model(5, [16,32,64,96,96], 3, [32,32,32],2)
+#seq.add_model(5, [16,32,64,96,96], 4, [64,32,24,16],2)
+#seq.add_model(5, [16,32,64,96,96], 4, [28,28,28,28],2)
 #
-##Alles konstant Anzahl filter variieren
-#seq.add_model(2,16,2,32,3)
-#seq.add_model(2,32,2,32,3)
-#seq.add_model(2,64,2,32,3)
-#seq.add_model(2,96,2,32,3)
-#
-## dense layer variieren
-#seq.add_model(2,64,1,32,3)
-#seq.add_model(2,64,2,32,3)
-#seq.add_model(2,64,3,32,3)
-#
-## Anzahl dense units
-#seq.add_model(2,64,2,16,3)
-#seq.add_model(2,64,2,32,3)
-#seq.add_model(2,64,2,64,3)
+#seq.add_model(2, [16,96],2, [32,32],3)
+#seq.add_model(2, [16,96],2, [48,24],3)
+#seq.add_model(2, [16,96],1, [64],3)
 
-seq.add_model
+seq.add_dnn(6,[70,60,50,40,30,20],1)
+seq.add_dnn(6,[70,60,50,40,30,20],2)
+seq.add_dnn(6,[70,60,50,40,30,20],3)
+seq.add_dnn(6,[70,60,50,40,30,20],4)
+seq.add_dnn(6,[70,60,50,40,30,20],4)
 
 seq.compileall()
 
-seq.trainall(my_training_batch_generator,my_validation_batch_generator, 0)
+seq.trainall(my_training_batch_generator,my_validation_batch_generator, False, 0)
 
--- code/testsequence.py
+++ code/testsequence.py
@@ -6,9 +6,9 @@ from keras.models import Sequential, Model, load_model
 from keras.layers import Input, Conv1D, Conv2D, MaxPooling1D, MaxPooling2D, Dense, Dropout, Activation, Flatten
 import matplotlib.pyplot as plt
 
-from keras.backend.tensorflow_backend import set_session
-from keras.backend.tensorflow_backend import clear_session
-from keras.backend.tensorflow_backend import get_session
+from tensorflow.compat.v1.keras.backend import set_session
+from tensorflow.compat.v1.keras.backend import clear_session
+from tensorflow.compat.v1.keras.backend import get_session
 
 from tensorflow.compat.v1 import ConfigProto
 from tensorflow.compat.v1 import InteractiveSession
@@ -25,9 +25,21 @@ class Testsequence:
 
     def __init__(self, testname, epochs):
         self.models = []
-        self.text_file = open("logs/test_" + testname + ".log", "w")
+        self.text_file = open("test_" + testname + ".log", "w")
         self.text_file.write('Log file for test ' + testname + '\n')
         self.epochs = epochs
+
+    def add_dnn(self, numdense, units,pooling):
+        
+        model = Sequential()
+        model.add(MaxPooling2D((pooling,pooling),input_shape=(512,512,3)))
+        model.add(Flatten())
+        for i in range(0,numdense):
+            model.add(Dense(units[i], activation='relu'))
+
+        model.add(Dense(3, activation='sigmoid'))
+
+        self.models.append(model)
 
     def add_model(self, numconvlayer, numfilters, numdense, numnodes, pooling):
 
@@ -41,8 +53,8 @@ class Testsequence:
         
         model.add(Flatten())
 
-        for i in range(1,numdense):
-            model.add(Dense(numnodes, activation='relu', kernel_initializer='he_uniform'))
+        for i in range(0,numdense):
+            model.add(Dense(numnodes[i], activation='relu', kernel_initializer='he_uniform'))
             
         model.add(Dense(3, activation='sigmoid'))
 
@@ -74,7 +86,7 @@ class Testsequence:
         for model in self.models:
             model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-    def trainall(self, traingenerator, valgenerator, safemodel):
+    def trainall(self, traingenerator, valgenerator, safe, safemodel):
         i = 0
         for model in self.models:
             self.text_file.write("Model " + str(i) + "\n")
@@ -90,9 +102,8 @@ class Testsequence:
                                           callbacks=[tensorboard_callback])
             end = time.time()
 
-            if(i == safemodel):
+            if(safe == True and i == safemodel):
                 model.save("my_model")
-                
 
             self.text_file.write('time for training: ' + str(end-start) + '\n')
 
@@ -101,13 +112,15 @@ class Testsequence:
             self.text_file.write("==============================\n")
             self.text_file.write("==============================\n")
 
-            plt.plot(history.history['acc'])
-            plt.plot(history.history['val_acc'])
+            plt.plot(history.history['accuracy'])
+            plt.plot(history.history['val_accuracy'])
             plt.title('model accuracy')
             plt.ylabel('accuracy')
             plt.xlabel('epoch')
             plt.legend(['training', 'validation'], loc='lower right')
             #plt.show()
-            plt.savefig("plots/model" + str(i) + ".pdf")
+            #plt.savefig("model" + str(i) + ".pdf")
             plt.close()
             self.reset_keras()
+
+        self.text_file.close()
