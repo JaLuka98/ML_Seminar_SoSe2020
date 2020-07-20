c4a266546f55e8dfabef58d6ba6a6e075f1d49c0
Tom Magorsch <tom@TmDebian>
2020-07-20 21:25:59 +0200

    code auf finales training vorbeerietet

--- code/test.py
+++ code/test.py
@@ -9,48 +9,35 @@ from keras.layers import Input, Conv1D, Conv2D, MaxPooling1D, MaxPooling2D, Dens
 from keras.layers.normalization import BatchNormalization
 from keras.utils import to_categorical
 
+import numpy as np
 
 from tensorflow.compat.v1 import ConfigProto
 from tensorflow.compat.v1 import InteractiveSession
 
 config = ConfigProto()
-config.gpu_options.per_process_gpu_memory_fraction = 0.75
+config.gpu_options.per_process_gpu_memory_fraction = 0.9
 config.gpu_options.allow_growth = True
 session = InteractiveSession(config=config)
 
 train_dir = "afhq/train"
-image_num = 14000
+image_num = 14500
 val_split = 0.3
 
 X_train_filenames, X_val_filenames, y_train, y_val = load_data(train_dir, image_num, val_split)
 
-batch_size = 5
+batch_size = 11
 
 my_training_batch_generator = Generator(X_train_filenames, y_train, batch_size)
 my_validation_batch_generator = Generator(X_val_filenames, y_val, batch_size)
 
 
-seq = Testsequence("Test1",50)
+seq = Testsequence("Reg",250)
 
-#seq.add_model(5, [16,32,64,96,96], 1, [128],2)
-#seq.add_model(5, [16,32,64,96,96], 2, [64,32],2)
-#seq.add_model(5, [16,32,64,96,96], 2, [48,48],2)
-#seq.add_model(5, [16,32,64,96,96], 3, [48,32,16],2)
-#seq.add_model(5, [16,32,64,96,96], 3, [32,32,32],2)
-#seq.add_model(5, [16,32,64,96,96], 4, [64,32,24,16],2)
-#seq.add_model(5, [16,32,64,96,96], 4, [28,28,28,28],2)
-#
-#seq.add_model(2, [16,96],2, [32,32],3)
-#seq.add_model(2, [16,96],2, [48,24],3)
-#seq.add_model(2, [16,96],1, [64],3)
+seq.add_model(5, [16,32,64,96,96], 1, [128],2,[0,0,0,0,0.1,0.1],0,0,2)
 
-seq.add_dnn(6,[70,60,50,40,30,20],1)
-seq.add_dnn(6,[70,60,50,40,30,20],2)
-seq.add_dnn(6,[70,60,50,40,30,20],3)
-seq.add_dnn(6,[70,60,50,40,30,20],4)
-seq.add_dnn(6,[70,60,50,40,30,20],4)
+seq.add_model(2, [16,96],1, [64],3,[0,0,0.1],0,0,1)
 
 seq.compileall()
 
-seq.trainall(my_training_batch_generator,my_validation_batch_generator, False, 0)
+seq.trainall(my_training_batch_generator,my_validation_batch_generator, True)
 
--- code/testsequence.py
+++ code/testsequence.py
@@ -10,8 +10,12 @@ from tensorflow.compat.v1.keras.backend import set_session
 from tensorflow.compat.v1.keras.backend import clear_session
 from tensorflow.compat.v1.keras.backend import get_session
 
+from tensorflow.keras import regularizers
+
 from tensorflow.compat.v1 import ConfigProto
 from tensorflow.compat.v1 import InteractiveSession
+
+import numpy as np
 
 import gc
 
@@ -40,21 +44,39 @@ class Testsequence:
         model.add(Dense(3, activation='sigmoid'))
 
         self.models.append(model)
-
-    def add_model(self, numconvlayer, numfilters, numdense, numnodes, pooling):
+    # numconvlayer: Anzahl convolutional layer (int)
+    # numfilters: Anzahl filter pro conv layer (array int)
+    # numdense: Anzahl dense layer (int)
+    # numnodes: Anzahl unit pro dense layer (array int)
+    # pooling: pooling size
+    # dopout: Dropout nach jedem conv und dense layer (array double) dorpout = 0 für kein dropout
+    # lam1: lambda für l1 regularisierung
+    # lam2: lambda für l2 regularisierung
+    # lastreglayers: anzahl der layer die mit l1 und l2 regularisiert werden (von hinten gezählt)
+    def add_model(self, numconvlayer, numfilters, numdense, numnodes, pooling, dropout, lam1, lam2, lastreglayers):
 
         model = Sequential()
         model.add(Conv2D(numfilters[0], (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same', input_shape=(512, 512, 3)))
         model.add(MaxPooling2D((pooling, pooling)))
 
         for i in range(1,numconvlayer):
-            model.add(Conv2D(numfilters[i], (3, 3), activation='relu'))
+            if(i >= lastreglayers):
+                model.add(Conv2D(numfilters[i], (3, 3), activation='relu', kernel_regularizer=regularizers.l1_l2(l1=lam1,l2=lam2)))
+            else:
+                model.add(Conv2D(numfilters[i], (3, 3), activation='relu'))
+            if not dropout[i] == 0:
+                model.add(Dropout(dropout[i]))
             model.add(MaxPooling2D((pooling, pooling)))
         
         model.add(Flatten())
 
         for i in range(0,numdense):
-            model.add(Dense(numnodes[i], activation='relu', kernel_initializer='he_uniform'))
+            if(i >= lastreglayers):
+                model.add(Dense(numnodes[i], activation='relu', kernel_initializer='he_uniform', kernel_regularizer=regularizers.l1_l2(l1=lam1,l2=lam2)))
+            else:
+                model.add(Dense(numnodes[i], activation='relu', kernel_initializer='he_uniform'))
+            if not dropout[i+numconvlayer] == 0:
+                model.add(Dropout(dropout[i]))
             
         model.add(Dense(3, activation='sigmoid'))
 
@@ -78,7 +100,7 @@ class Testsequence:
             
             # use the same config as you used to create the session
             config = ConfigProto()
-            config.gpu_options.per_process_gpu_memory_fraction = 0.75
+            config.gpu_options.per_process_gpu_memory_fraction = 0.9
             config.gpu_options.allow_growth = True
             set_session(InteractiveSession(config=config))
 
@@ -86,7 +108,7 @@ class Testsequence:
         for model in self.models:
             model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-    def trainall(self, traingenerator, valgenerator, safe, safemodel):
+    def trainall(self, traingenerator, valgenerator, safe):
         i = 0
         for model in self.models:
             self.text_file.write("Model " + str(i) + "\n")
@@ -97,30 +119,24 @@ class Testsequence:
             tensorboard_callback = TensorBoard(logdir)
 
             start = time.time()
-            history = model.fit_generator(traingenerator, steps_per_epoch=128, 
-                                          validation_data=valgenerator, validation_steps=128, epochs=self.epochs, verbose=1, 
+            history = model.fit_generator(traingenerator, steps_per_epoch=300, 
+                                          validation_data=valgenerator, validation_steps=300, epochs=self.epochs, verbose=1, 
                                           callbacks=[tensorboard_callback])
             end = time.time()
 
-            if(safe == True and i == safemodel):
-                model.save("my_model")
+            if(safe == True):
+                model.save("model"+str(i))
 
             self.text_file.write('time for training: ' + str(end-start) + '\n')
 
-            i+=1
+
             self.text_file.write("==============================\n")
             self.text_file.write("==============================\n")
             self.text_file.write("==============================\n")
 
-            plt.plot(history.history['accuracy'])
-            plt.plot(history.history['val_accuracy'])
-            plt.title('model accuracy')
-            plt.ylabel('accuracy')
-            plt.xlabel('epoch')
-            plt.legend(['training', 'validation'], loc='lower right')
-            #plt.show()
-            #plt.savefig("model" + str(i) + ".pdf")
-            plt.close()
+            np.savetxt(str(i)+"history_loss.txt",np.vstack((np.array(history.history['accuracy']),np.array(history.history['val_accuracy']),history.history['loss'],history.history['val_loss'])).T)
+
+            i+=1
             self.reset_keras()
 
         self.text_file.close()
