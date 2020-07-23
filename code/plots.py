d09a7129bd1db1880287b373f22f87c506bbec3a
Jan Lukas Spaeh <janlukas.spaeh@tu-dortmund.de>
2020-07-23 16:20:38 +0200

    Done a lot of work, confusion and roc looking quite nice

--- code/plots.py
+++ code/plots.py
@@ -7,6 +7,47 @@ from loader import load_data
 import matplotlib.pyplot as plt
 
 from sklearn.metrics import confusion_matrix
+
+# For plot_confusion_matrix
+import itertools
+import matplotlib.cm as cm
+
+# Acknowledgement to Olaf Nackenhorst Exercise 4 ML Seminar summer term 2020, TU Dortmund
+def plot_confusion_matrix(cm, classes,
+                          normalize=True,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues,
+                          colorbar=True):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    if colorbar: plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+    # matplotlib 3.1.1 broke inverted axes with fixed ticks :(
+    # Maybe this is not necessary in other version. Check it at the end!
+    plt.ylim([2.5,-0.5])
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    # Loop over data dimensions and create text annotations.
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    #plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+
 #########################################
 ######### 1) Input Data Plots ###########
 #########################################
@@ -183,38 +224,78 @@ plt.clf()
 ########### 5) Performance Plot #########
 #########################################
 
-import itertools
-import matplotlib.cm as cm
+# Hier noch ein mal, weil ich nichts bei Punkt 3 verändern will. Mergen soll einfach sein
+i, label_2, cat_2, dog_2, wildlife_2 = np.genfromtxt('2layer_predictions.txt',unpack=True)
+i, label_7, cat_7, dog_7, wildlife_7 = np.genfromtxt('7layer_predictions.txt',unpack=True)
 
-def plot_confusion_matrix(cm, classes,
-                          normalize=True,
-                          title='Confusion matrix',
-                          cmap=plt.cm.Blues):
-    """
-    This function prints and plots the confusion matrix.
-    Normalization can be applied by setting `normalize=True`.
-    """
-    plt.imshow(cm, interpolation='nearest', cmap=cmap)
-    plt.title(title)
-    plt.colorbar()
-    tick_marks = np.arange(len(classes))
-    plt.xticks(tick_marks, classes, rotation=45)
-    plt.yticks(tick_marks, classes)
+Y_pred_2 = np.vstack((dog_2, wildlife_2, cat_2)).T # Now it is in the common convention (1500,3)
+# Number of examples, number of classes
+Y_cls_2 = np.argmax(Y_pred_2, axis = 1)
+conf_mat_2 = confusion_matrix(label_2, Y_cls_2)
 
-    if normalize:
-        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+# Do the same for the seven layer model
+Y_pred_7 = np.vstack((dog_7, wildlife_7, cat_7)).T # Now it is in the common convention (1500,3)
+# Number of examples, number of classes
+Y_cls_7 = np.argmax(Y_pred_7, axis = 1)
+conf_mat_7 = confusion_matrix(label_7, Y_cls_7)
 
-    # Loop over data dimensions and create text annotations.
-    fmt = '.2f' if normalize else 'd'
-    thresh = cm.max() / 2.
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        plt.text(j, i, format(cm[i, j], fmt),
-                 horizontalalignment="center",
-                 color="white" if cm[i, j] > thresh else "black")
+# plot the confusion matrices side by side
+plt.subplots_adjust(wspace=0.5,hspace=0.5)
+plt.subplot(121)
+plot_confusion_matrix(conf_mat_2, classes = ['dog', 'wildlife', 'cat'], normalize=False, title='Two Layers', colorbar=False)
+plt.subplot(122)
+plot_confusion_matrix(conf_mat_7, classes = ['dog', 'wildlife', 'cat'], normalize=False, title='Seven Layers', colorbar=False)
+plt.tight_layout()
+plt.savefig('plots/confusion_matrix.pdf')
+plt.clf()
 
-    plt.tight_layout()
-    plt.ylabel('True label')
-    plt.xlabel('Predicted label')
+##################################################
+########### 5) Understanding performance #########
+##################################################
 
-# with confusion matrix
-#confusion_matrix(y_true, y_pred)
+import matplotlib.pyplot as plt
+from sklearn import svm, datasets
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import label_binarize
+from sklearn.metrics import roc_curve, auc
+from sklearn.multiclass import OneVsRestClassifier
+from itertools import cycle
+
+def lin(x,a,b):
+    return a*x+b
+
+def plot_roc_curves(label, Y_pred, title='ROC Curves'):
+    # Binarize the output
+    Y_one_hot = label_binarize(label, classes=[0, 1, 2])
+
+    fpr = dict()
+    tpr = dict()
+    roc_auc = dict()
+    x_plot = np.linspace(0.001,1,1000)
+    for i in range(3):
+        fpr[i], tpr[i], _ = roc_curve(Y_one_hot[:, i], Y_pred[:, i])
+        roc_auc[i] = auc(fpr[i], tpr[i])
+        colors = cycle(['blue', 'red', 'green'])
+    for i, color in zip(range(3), colors):
+        plt.plot(fpr[i], tpr[i], color=color,
+                label='ROC class {0} (AUC = {1:0.3f})'
+                ''.format(i, roc_auc[i]))
+        plt.plot(x_plot, lin(x_plot, 1,0), 'k--')
+        plt.xlim([0.001, 1.0])
+        plt.ylim([0.0, 1.05])
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.xscale('log')
+        plt.title(title)
+        plt.tight_layout()
+        plt.legend(loc="best")
+        plt.grid()
+
+
+#plt.subplots_adjust(wspace=0.5,hspace=0.5)
+plt.figure(figsize=(9,4.5))
+plt.subplot(121)
+plot_roc_curves(label_2, Y_pred_2, title='Two Layers')
+plt.subplot(122)
+plot_roc_curves(label_7, Y_pred_7, title='Seven Layers')
+plt.savefig('plots/ROC_Curves.pdf')
