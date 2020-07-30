524369967ac5587c0815ce583e4ce450cca35a98
TomMago <tom.magorsch@tu-dortmund.de>
2020-07-30 14:55:31 +0200

    Paar Dateien gelöscht und ReadMe hinzugefügt

--- code/errors.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-i, label, cat, wildlife, dog = np.genfromtxt('predictions/7layer_predictions_test.txt',unpack=True)
-
-print(max(wildlife[label==2]))
